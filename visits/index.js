265b4684e711f301f83e13fec36b89c834399fb5
t03jam8 <grovejam@gmail.com>
2019-04-09 23:28:57 +0200

    using docker compose

--- visits/index.js
+++ visits/index.js
@@ -1,11 +1,17 @@
 const express = require("express")
 const redis = require("redis")
+const process = require("process")
 
 const app = express()
-const client = redis.createClient()
+const client = redis.createClient({
+  host: "redis-server", // see docker-compose.yml name of the redis service
+  port: 6379 // redis default port
+})
+
 client.set("visits", 0)
 
 app.get("/", (req, res) => {
+  process.exit(0) // cause crash
   client.get("visits", (err, visits) => {
     res.send("Number of visits is " + visits)
     client.set("visits", parseInt(visits) + 1)
